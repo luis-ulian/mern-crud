98a8e97cb972075754c140fd0ac5f9ba3258875e
Luís Ulian <luissteam44@gmail.com>
2025-04-03 09:26:00 -0300

    product card design in progress (learning more about css)

--- frontend/src/Components/ProductCard.jsx
+++ frontend/src/Components/ProductCard.jsx
@@ -5,13 +5,15 @@ const ProductCard = ({product}) => {
   return (
     <div className='card'>
         <img className="productImg" src={product.image} alt={product.name} />
-        <div className='cardText'>
-          <h3 className='productName'>{product.name}</h3>
-          <p1 className='productPrice'>R$ {product.price}</p1>
-        </div>
-        <div className="iconController">
-          <button src="edit.png" className="editIcon" ></button>
-          <button src="delete.png" className="deleteIcon" ></button>
+        <div className='productFields'>
+          <div className='cardText'>
+            <h1 className='productName'>{product.name}</h1>
+            <p1 className='productPrice'>R$ {Intl.NumberFormat("pt-BR", {currency: "BRL"}).format(product.price)}</p1>
+          </div>
+          <div className="iconController">
+            <input type="image" src="edit.png" className='editIcon '/>
+            <input type="image" src="delete.png" className='deleteIcon' />
+          </div>
         </div>
     </div>
   )
